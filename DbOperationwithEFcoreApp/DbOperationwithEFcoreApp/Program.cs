8bac5f6d2153857634ae722fd33b2dfc4b60becf
TUSHAR PAKHIDDE <TUSHAR PAKHIDDE@LAPTOP-IDI3VPEM>
2026-08-26 20:12:41 +0530

    install EF core and setup Db Context class

--- DbOperationwithEFcoreApp/DbOperationwithEFcoreApp/Program.cs
+++ DbOperationwithEFcoreApp/DbOperationwithEFcoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationwithEFcoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationwithEFcoreApp
 {
     public class Program
@@ -7,6 +10,9 @@ namespace DbOperationwithEFcoreApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
+          
+            
             // Add services to the container.
 
             builder.Services.AddControllers();
